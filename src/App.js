4b6c2132a7e1eee9fec4951609525982fa668d04
Alston Pough <alstonpough@Alstons-MacBook-Pro.local>
2022-05-12 12:57:15 -0400

    gallery

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
-
   return (
     <div>
-      <Nav />
+      <Nav></Nav>
       <main>
-          <About />        
+        <About></About>
       </main>
     </div>
   );
--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,19 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-const { asFragment } = render(<About />);
+afterEach(cleanup);
 
 describe('About component', () => {
-    // first test
-    it('renders', () => {
-        render(<About />);
-    });
-
-    it('matches snapshot DOM node structure', () => {
-    // render About
+  it('renders', () => {
+    render(<About />);
   });
-});
-
-expect(asFragment()).toMatchSnapshot();
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
